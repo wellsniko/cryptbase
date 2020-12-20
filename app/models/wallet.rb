94bdbac6c25282fd7e9a428270ece787018d8235
Niko Wells <wellsniko@gmail.com>
2020-12-20 15:24:45 -0800

    troubleshooting heroku 2

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,13 +1,13 @@
 class Wallet < ApplicationRecord
-	# validates :wallet_address, :user_id,  presence: true
+	validates :wallet_address, :user_id,  presence: true
 	
-	# belongs_to :user,
-	# 	foreign_key: :user_id,
-	# 	class_name: :User
+	belongs_to :user,
+		foreign_key: :user_id,
+		class_name: :User
 
-	# has_many :orders, 
-	# 	foreign_key: :wallet_id,
-	# 	class_name: :Order
+	has_many :orders, 
+		foreign_key: :wallet_id,
+		class_name: :Order
 	
 	def self.find_wallet(user_id, coin_id)
         wallet = Wallet.find_by(user_id: user_id, coin_id: coin_id)				
