b8a1fe9eafb15e8b755c663a308a352997207e11
Niko Wells <wellsniko@gmail.com>
2021-02-26 20:33:50 -0800

    orders formatting and details

--- frontend/components/dashboard/order_index_item.jsx
+++ frontend/components/dashboard/order_index_item.jsx
@@ -14,9 +14,9 @@ const OrderIndexItem = props => {
         </div>
         <div>
           <h4 className="order-h4">{order.transaction_type === "BUY" ? `Bought ${props.coin.name}` : `Sold ${props.coin.name}`}</h4>
-          <p className="order-p">{order.transaction_type === "BUY" ? `+${Number(order.quantity).toFixed(4)}` : `-${Number(order.quantity).toFixed(4)}`}&nbsp;on&nbsp;{new Date (order.created_at).toLocaleString("en-US", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric"})}</p>
+          <p className="order-p">{order.transaction_type === "BUY" ? `+${Number(order.quantity).toFixed(4)}` : `-${Number(order.quantity).toFixed(4)}`}&nbsp;on&nbsp;{new Date (order.created_at).toLocaleString("en-US", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric"})} at<br/>{Number(order.price).toLocaleString('en-US', {style: 'currency',currency: 'USD'})}</p>
         </div>
-        <label className="order-right">{Number(order.price).toLocaleString('en-US', {style: 'currency',currency: 'USD'})}</label>
+        <label className="order-right">{(Number(order.price) * Number(order.quantity)).toLocaleString('en-US', {style: 'currency',currency: 'USD'})}</label>
         {/* {Number(order.quantity).toFixed(4)}&nbsp;&nbsp;&nbsp;&nbsp;{order.coin_id}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{new Date (order.created_at).toLocaleString("en-US", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric"})} */}
       </div>
   )
